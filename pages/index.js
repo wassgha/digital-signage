bbf506ba75af2a73d180b2db3baea50dfc1ccde5
Wassim Gharbi <wassgha@gmail.com>
2019-02-24 11:38:35 -0500

    Added linting to the project

--- pages/index.js
+++ pages/index.js
@@ -45,7 +45,7 @@ Index.getInitialProps = async function() {
   const res = await fetch('https://api.tvmaze.com/search/shows?q=batman')
   const data = await res.json()
 
-  console.log(`Show data fetched. Count: ${data.length}`)
+  // console.log(`Show data fetched. Count: ${data.length}`)
 
   return {
     shows: data
