d6be0215f7c938d776e896279c41591836fa69c6
Alexander Andrews <alexander.e.andrews.ce@gmail.com>
2019-02-28 16:23:27 -0500

    WOrk in progress, moving over to ym desktop

--- pages/mongoTest.js
+++ pages/mongoTest.js
@@ -1,32 +1,14 @@
-import Layout from "../components/Layout.js"
-import Markdown from "react-markdown"
-import React, { Component } from "react"
-import fetch from "isomorphic-unfetch"
+import Layout from '../components/Layout.js'
+import Markdown from 'react-markdown'
+import fetch from 'isomorphic-unfetch'
 
-const mongoose = require("mongoose")
+const mongoose = require('mongoose')
 
-class MongoTest extends Component {
-  //eslint-disable-next-line no-console
-  //console.log(this.props)
-  render() {
-    //eslint-disable-next-line no-console
-    console.log(this.props)
-    return (
-      <Layout>
-        <p>This is the about page</p>
-      </Layout>
-    )
-  }
-}
+const mongoTest = props => (
+  <Layout>
+    
+  </Layout>
+)
 
-Post.getInitialProps = async function(context) {
-  const { id } = context.query
-  const res = await fetch(`https://api.tvmaze.com/shows/${id}`)
-  const show = await res.json()
 
-  // console.log(`Fetched show: ${show.name}`)
-
-  return { show }
-}
-
-export default MongoTest
+export default mongoTest
