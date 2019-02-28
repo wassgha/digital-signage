9c8df650fbf8e02f9533d3d3344c13ec98f0651e
Alexander Andrews <alexander.e.andrews.ce@gmail.com>
2019-02-28 16:23:27 -0500

    WOrk in progress, moving over to ym desktop

--- server.js
+++ server.js
@@ -1,15 +1,53 @@
 const express = require('express')
 const next = require('next')
+const mongoose = require('mongoose')
 
 const dev = process.env.NODE_ENV !== 'production'
 const port = process.env.PORT || 3000
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
+const devF = require('./.dev.js')
+
 app
   .prepare()
   .then(() => {
     const server = express()
+
+    mongoose.connect(devF.MONGO_URL)
+    var db = mongoose.connection
+    // eslint-disable-next-line no-console
+    db.on('error', console.error.bind(console, 'connection error:'))
+    var data
+    db.once('open', function() {
+      // we're connected!
+      const schemass = require('./models/simpleModel.js')
+      var testSchema = schemass.GROUP_MEMBER
+      var mirjan = new testSchema({name: 'Mirjan', title: 'Doritoes and MTn Dew Code Red Fetcher'})
+      mirjan.save(function (err, mirjan){
+        // eslint-disable-next-line no-console
+        if (err) return console.error(err)
+        // eslint-disable-next-line no-console
+      })
+
+      testSchema.find(function (err, mirjans){
+        // eslint-disable-next-line no-console
+        if(err) return console.error(err)
+        // eslint-disable-next-line no-console
+        data = mirjans
+      })
+      // eslint-disable-next-line no-console
+      console.log(mirjan.name)
+      // eslint-disable-next-line no-console
+      console.log('yolo nerds')
+    })
+
+    
+    server.get('/mongoTest', (req, res) => {
+      
+      app.render(req, res, '/mongoTest', data)
+    })
+
 
     server.get('/p/:id', (req, res) => {
       const actualPage = '/post'
@@ -32,3 +70,4 @@ app
     console.error(ex.stack)
     process.exit(1)
   })
+
