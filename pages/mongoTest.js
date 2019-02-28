4cadf9ee7ca3da3fec46a590669ffdd23469d3b1
Alexander Andrews <alexander.e.andrews.ce@gmail.com>
2019-02-28 16:23:27 -0500

    WOrk in progress, moving over to ym desktop

---
+++ pages/mongoTest.js
@@ -0,0 +1,14 @@
+import Layout from '../components/Layout.js'
+import Markdown from 'react-markdown'
+import fetch from 'isomorphic-unfetch'
+
+const mongoose = require('mongoose')
+
+const mongoTest = props => (
+  <Layout>
+    
+  </Layout>
+)
+
+
+export default mongoTest
