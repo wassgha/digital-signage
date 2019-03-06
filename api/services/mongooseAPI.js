75beb1a683e91071d1307e39630fd743c2ede4d9
p0wndizz7e <andrewsa@lafayette.edu>
2019-03-06 12:45:01 -0500

    Added mongoose API file, it conectys to the DB, gonna see what functionality because of mongoose is needed

---
+++ api/services/mongooseAPI.js
@@ -0,0 +1,49 @@
+const devF = require('../../.dev.js')
+const mongoose = require('mongoose')
+
+var db
+class mongooseAPI{
+    constructor(){  
+    mongoose.connect(devF.MONGO_URL)
+    this.db = mongoose.connection
+    // eslint-disable-next-line no-console
+    this.db.on('error', console.error.bind(console, 'connection error:'))
+    }
+
+    getMirjans(){
+        
+        var data
+        // eslint-disable-next-line no-console
+        this.db.on('error', console.error.bind(console, 'connection error:'))
+        
+        this.db.once('open', function() {
+            // we're connected!
+            
+            const schemass = require('../models/simpleModel.js')
+            var testSchema = schemass.GROUP_MEMBER
+            var mirjan = new testSchema(
+                {name: 'Mirjan', title: 'Doritoes and MTn Dew Code Red Fetcher'})
+            mirjan.save(function (err, mirjan){
+              // eslint-disable-next-line no-console
+              if (err) return console.error(err)
+              // eslint-disable-next-line no-console
+            })
+      
+            testSchema.find(function (err, mirjans){
+              // eslint-disable-next-line no-console
+              if(err) return console.error(err)
+              // eslint-disable-next-line no-console
+              data = mirjans
+            })
+            // eslint-disable-next-line no-console
+            console.log(mirjan.name)
+            // eslint-disable-next-line no-console
+            console.log(data)
+            // eslint-disable-next-line no-console
+            console.log('yolo nerds')
+          })
+          return data
+    }
+}
+
+module.exports = mongooseAPI
