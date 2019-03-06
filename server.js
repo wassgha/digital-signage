5c6dc64b489738b825daf2e8f4324b11e383ecad
p0wndizz7e <andrewsa@lafayette.edu>
2019-03-06 12:45:01 -0500

    Added mongoose API file, it conectys to the DB, gonna see what functionality because of mongoose is needed

--- server.js
+++ server.js
@@ -22,41 +22,6 @@ app
     })
 
 
-    mongoose.connect(devF.MONGO_URL)
-    var db = mongoose.connection
-    // eslint-disable-next-line no-console
-    db.on('error', console.error.bind(console, 'connection error:'))
-    var data
-    db.once('open', function() {
-      // we're connected!
-      const schemass = require('./api/models/simpleModel.js')
-      var testSchema = schemass.GROUP_MEMBER
-      var mirjan = new testSchema({name: 'Mirjan', title: 'Doritoes and MTn Dew Code Red Fetcher'})
-      mirjan.save(function (err, mirjan){
-        // eslint-disable-next-line no-console
-        if (err) return console.error(err)
-        // eslint-disable-next-line no-console
-      })
-
-      testSchema.find(function (err, mirjans){
-        // eslint-disable-next-line no-console
-        if(err) return console.error(err)
-        // eslint-disable-next-line no-console
-        data = mirjans
-      })
-      // eslint-disable-next-line no-console
-      console.log(mirjan.name)
-      // eslint-disable-next-line no-console
-      console.log('yolo nerds')
-    })
-
-    
-    server.get('/mongoTest', (req, res) => {
-      
-      app.render(req, res, '/mongoTest', data)
-    })
-
-
     server.get('/p/:id', (req, res) => {
       const actualPage = '/post'
       const queryParams = { id: req.params.id }
