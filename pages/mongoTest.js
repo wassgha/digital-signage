3f9a448166858740db0d93a44cce7ba680b12b41
p0wndizz7e <andrewsa@lafayette.edu>
2019-03-06 12:45:01 -0500

    Added mongoose API file, it conectys to the DB, gonna see what functionality because of mongoose is needed

--- pages/mongoTest.js
+++ pages/mongoTest.js
@@ -19,4 +19,14 @@ class MongoTest extends Component {
   }
 }
 
+Post.getInitialProps = async function(context) {
+  const { id } = context.query
+  const res = await fetch(`https://api.tvmaze.com/shows/${id}`)
+  const show = await res.json()
+
+  // console.log(`Fetched show: ${show.name}`)
+
+  return { show }
+}
+
 export default MongoTest
