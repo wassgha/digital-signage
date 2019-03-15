dd4e21c7ab826d0c922ccf13b7f712ecf7004188
p0wndizz7e <andrewsa@lafayette.edu>
2019-03-15 13:49:11 -0400

    Still Cleaning things up

--- api/services/mongooseAPI.js
+++ api/services/mongooseAPI.js
@@ -1,7 +1,6 @@
 const devF = require("../../.dev.js")
 const mongoose = require("mongoose")
-const schemas = require("../models/simpleModel")
-const slideSchemas = require("../models/sildeshowSchema")
+//const schemas = require("../models/simpleModel")
 
 
 var db
@@ -12,86 +11,6 @@ class mongooseAPI {
     this.db = mongoose.connection
     // eslint-disable-next-line no-console
     this.db.on("error", console.error.bind(console, "connection error:"))
-  }
-
-  dropDatabase(){
-    // eslint-disable-next-line no-console
-    console.log("Lets see whats happening")
-  }
-
-  clearSchema(schemalModel){
-    var testSchema = schemas.GROUP_MEMBER
-    schemalModel.deleteMany({}, function(err){
-      // eslint-disable-next-line no-console
-      if (err) return console.error(err)
-    })
-  }
-
-  clearMirjans(){
-    var testSchema = schemas.GROUP_MEMBER
-    // eslint-disable-next-line no-console
-    console.log("Frag noobs")
-    testSchema.deleteMany({}, function(err){
-      // eslint-disable-next-line no-console
-      if (err) return console.error(err)
-      // eslint-disable-next-line no-console
-    })
-  }
-
-  clearSlides(){
-
-    var slide = slideSchemas.SLIDE
-    var slideShow = slideSchemas.SLIDESHOW
-    slide.deleteMany({}, function(err){
-      // eslint-disable-next-line no-console
-      if (err) return console.error(err)
-      // eslint-disable-next-line no-console
-      console.log("Deleted slides")
-    })
-    slideShow.deleteMany({}, function(err){
-      // eslint-disable-next-line no-console
-      if (err) return console.error(err)
-      // eslint-disable-next-line no-console
-      console.log("deleted slideshows")
-    })
-  }
-
-  getMirjans() {
-    var data
-    // eslint-disable-next-line no-console
-    this.db.on("error", console.error.bind(console, "connection error:"))
-    /*
-     *this.db.on("open", function() {
-     * we're connected!
-     */
-      
-      var testSchema = schemas.GROUP_MEMBER
-      var mirjan = new testSchema({
-        name: "Mirjan",
-        title: "Doritoes and MTn Dew Code Red Fetcher"
-      })
-      mirjan.save(function(err, mirjan) {
-        // eslint-disable-next-line no-console
-        if (err) return console.error(err)
-        // eslint-disable-next-line no-console
-      })
-
-      testSchema.find(function(err, mirjans) {
-        // eslint-disable-next-line no-console
-        if (err) return console.error(err)
-        // eslint-disable-next-line no-console
-        console.log(mirjans)
-      })
-      
-      // eslint-disable-next-line no-console
-      console.log(mirjan.name)
-      // eslint-disable-next-line no-console
-      console.log()
-      // eslint-disable-next-line no-console
-      console.log("yolo nerds")
-      
-    //})
-    return data
   }
 }
 
