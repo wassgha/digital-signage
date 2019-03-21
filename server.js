59cd067b6d3f0c428e222fdce63bf852f1ca806f
mirjan <nezam@lafayette.edu>
2019-03-21 01:16:18 -0400

    Completed file upload api (No datbase support yet)

--- server.js
+++ server.js
@@ -1,82 +1,55 @@
-const express = require("express");
-const next = require("next");
-const bodyParser = require("body-parser");
+const express = require("express")
+const next = require("next")
 
-const dev = process.env.NODE_ENV !== "production";
-const port = process.env.PORT || 3000;
-const app = next({ dev });
-const handle = app.getRequestHandler();
+const dev = process.env.NODE_ENV !== "production"
+const port = process.env.PORT || 3000
+const app = next({ dev })
+const handle = app.getRequestHandler()
+var pathForUploadedFiles = []
 
 app
   .prepare()
   .then(() => {
-    const server = express();
+    const server = express()
 
     server.get("/p/:id", (req, res) => {
-      const actualPage = "/post";
-      const queryParams = { id: req.params.id };
-      app.render(req, res, actualPage, queryParams);
-    });
+      const actualPage = "/post"
+      const queryParams = { id: req.params.id }
+      app.render(req, res, actualPage, queryParams)
+    })
 
-    server.post("/upload", (req, res) => {
-      const multer = require("multer");
-
-      const handleError = (err, res) => {
-        res
-          .status(500)
-          .contentType("text/plain")
-          .end("Oops! Something went wrong!");
-      };
-
-      const upload = multer({
-        dest: "/path/to/temporary/directory/to/store/uploaded/files"
-        // you might also want to set some limits: https://github.com/expressjs/multer#limits
-      });
-
-      app.post(
-        "/upload",
-        upload.single(
-          "file" /* name attribute of <file> element in your form */
-        ),
-        (req, res) => {
-          const tempPath = req.file.path;
-          const targetPath = path.join(__dirname, "./uploads/image.png");
-
-          if (path.extname(req.file.originalname).toLowerCase() === ".png") {
-            fs.rename(tempPath, targetPath, err => {
-              if (err) return handleError(err, res);
-
-              res
-                .status(200)
-                .contentType("text/plain")
-                .end("File uploaded!");
-            });
-          } else {
-            fs.unlink(tempPath, err => {
-              if (err) return handleError(err, res);
-
-              res
-                .status(403)
-                .contentType("text/plain")
-                .end("Only slideshow uploads are allowed");
-            });
-          }
+    server.post("/api/slide/upload", (req, res) => {
+      //handy npm package that parses form data
+      const formidable = require("formidable")
+      var form = new formidable.IncomingForm()
+      form.uploadDir = "./Uploaded_Slides"
+      form.keepExtensions = true
+      form.multiples = false
+      //parse the sent data and save it, as well as save the path for alex to work his database magic
+      form.parse(req, (err, fields, files) => {
+        if (err) {
+          res.json({
+            result: "Failed!",
+            message: "Cannot upload file, Error: ${err}"
+          })
         }
-      );
-    });
+        const filePath = files.data.path
+        pathForUploadedFiles.push(filePath)
+      })
+    })
 
     server.get("*", (req, res) => {
-      return handle(req, res);
-    });
+      return handle(req, res)
+    })
 
     server.listen(port, err => {
-      if (err) throw err;
+      if (err) throw err
       // eslint-disable-next-line
-      console.log("> Ready on http://localhost:" + port);
-    });
+      console.log("> Ready on http://localhost:" + port)
+    })
   })
   .catch(ex => {
     // eslint-disable-next-line
-    console.error(ex.stack);
-    process.exit(1);
-  });
+    console.error(ex.stack)
+    process.exit(1)
+  })
