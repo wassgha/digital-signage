957e3dee065640d075ee381089666866767ee56d
Wassim Gharbi <wassgha@gmail.com>
2019-03-22 16:15:30 +0100

    Serves uploaded files

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require("express")
-const next = require("next")
-const formidable = require("formidable")
+const express = require('express')
+const next = require('next')
+const formidable = require('formidable')
 
-const dev = process.env.NODE_ENV !== "production"
+const dev = process.env.NODE_ENV !== 'production'
 const port = process.env.PORT || 3000
 const app = next({ dev })
 const handle = app.getRequestHandler()
@@ -12,37 +12,39 @@ app
   .then(() => {
     const server = express()
 
-    server.get("/p/:id", (req, res) => {
-      const actualPage = "/post"
+    server.get('/p/:id', (req, res) => {
+      const actualPage = '/post'
       const queryParams = { id: req.params.id }
       app.render(req, res, actualPage, queryParams)
     })
 
-    server.post("/api/slide/upload", (req, res) => {
+    server.post('/api/slide/upload', (req, res) => {
       var form = new formidable.IncomingForm()
-      form.uploadDir = "./uploads"
+      form.uploadDir = './uploads'
       form.keepExtensions = true
       form.multiples = false
       //parse the sent data and save it + the path for alex
       form.parse(req, (err, fields, files) => {
         if (err) {
           res.json({
-            result: "error",
-            message: "Cannot upload file, Error: ${err}"
+            result: 'error',
+            message: 'Cannot upload file, Error: ${err}'
           })
         }
         const filePath = files.data.path
       })
     })
 
-    server.get("*", (req, res) => {
+    server.use('/uploads', express.static('uploads'))
+
+    server.get('*', (req, res) => {
       return handle(req, res)
     })
 
     server.listen(port, err => {
       if (err) throw err
       // eslint-disable-next-line
-      console.log("> Ready on http://localhost:" + port)
+      console.log('> Ready on http://localhost:' + port)
     })
   })
   .catch(ex => {
