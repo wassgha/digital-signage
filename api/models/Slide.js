17006c40fdf74de52cc692a2e782f1e6dffa3db2
Wassim Gharbi <wassgha@gmail.com>
2019-03-28 22:54:00 -0400

    Rewrote the API code

--- api/models/Slide.js
+++ api/models/Slide.js
@@ -1,13 +1,17 @@
-const mongoose = require("mongoose")
+const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const SLIDE = new Schema({
-  data: { type: String, default: "https://i.imgur.com/3MlKktU.jpg" },
-  //data: { type: Schema.Types.ObjectId, ref: "URL" },
-  type: { type: String, default: "photo", enum: ["photo", "web", "youtube", "video"] },
+const Slide = new Schema({
+  slideshow: { type: Schema.Types.ObjectId, ref: 'Slideshow' },
+  data: { type: String, default: 'https://i.imgur.com/3MlKktU.jpg' },
+  type: {
+    type: String,
+    default: 'photo',
+    enum: ['photo', 'web', 'youtube', 'video']
+  },
   title: { type: String },
   description: { type: String },
   duration: { type: Number, default: 5, min: 1 }
 })
 
-module.exports = mongoose.model("SLIDE", SLIDE)
+module.exports = mongoose.model('Slide', Slide)
--- server.js
+++ server.js
@@ -1,70 +1,53 @@
 /* eslint-disable multiline-comment-style */
-const express = require("express")
-const next = require("next")
-const formidable = require("formidable")
-const mongoose = require("mongoose")
+const express = require('express')
+const next = require('next')
+const mongoose = require('mongoose')
 
-const dev = process.env.NODE_ENV !== "production"
+const dev = process.env.NODE_ENV !== 'production'
 const port = process.env.PORT || 3000
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
-const dbAPI = require("./api/services/mongooseAPI")
+const Keys = require('./keys')
 
-const devF = require("./.dev.js")
-
-const slidesAPI = require("./api/services/slideshowMongooseAPI")
+const apiRoutes = require('./api/routes')
 
 app
   .prepare()
   .then(() => {
     const server = express()
-    let db = new dbAPI()
 
-    server.get("/mongoTest", (req, res) => {
-      async function ryan() {
-        // eslint-disable-next-line no-console
-        console.log("Inside ryan")
-        var idNum = await slidesAPI.returnSlideShows()
-        // eslint-disable-next-line no-console
-        console.log(idNum)
-        //slidesAPI.deleteSlideShow(idNum[0]._id)
-      }
-      ryan()
-    })
-    server.get("/p/:id", (req, res) => {
-      const actualPage = "/post"
-      const queryParams = { id: req.params.id }
-      app.render(req, res, actualPage, queryParams)
+    // Allows for cross origin domain request:
+    server.use(function(req, res, next) {
+      res.header('Access-Control-Allow-Origin', '*')
+      res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+      next()
     })
 
-    server.post("/api/slide/upload", (req, res) => {
-      var form = new formidable.IncomingForm()
-      form.uploadDir = "./uploads"
-      form.keepExtensions = true
-      form.multiples = false
-      //parse the sent data and save it + the path for alex
-      form.parse(req, (err, fields, files) => {
-        if (err) {
-          res.json({
-            result: "error",
-            message: "Cannot upload file, Error: ${err}"
-          })
-        }
-        const filePath = files.data.path
-      })
-    })
+    // MongoDB
+    mongoose.Promise = Promise
+    mongoose.connect(
+      Keys.MONGODB_URI,
+      { useNewUrlParser: true }
+    )
+    const db = mongoose.connection
+    db.on('error', console.error.bind(console, 'connection error:'))
 
-    server.use("/uploads", express.static("uploads"))
+    // API routes
+    server.use('/api/v1', apiRoutes)
 
-    server.get("*", (req, res) => {
+    // Static routes
+    server.use('/uploads', express.static('uploads'))
+
+    // Next.js routes
+    server.get('*', (req, res) => {
       return handle(req, res)
     })
 
     server.listen(port, err => {
       if (err) throw err
       // eslint-disable-next-line
-      console.log("> Ready on http://localhost:" + port)
+      console.log('> Ready on http://localhost:' + port)
     })
   })
   .catch(ex => {
