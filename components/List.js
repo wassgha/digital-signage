f4e4357945625c38ecb60ac2343d89f4529cb639
Wassim Gharbi <wassgha@gmail.com>
2019-04-05 22:38:50 -0400

    Better styles to the list + video and web thumbnails

--- components/List.js
+++ components/List.js
@@ -2,7 +2,8 @@ import { Component } from 'react'
 import React from 'react'
 import { SortableContainer, SortableElement, arrayMove } from 'react-sortable-hoc'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faClock, faTrash, faEdit } from '@fortawesome/free-solid-svg-icons'
+import { faClock } from '@fortawesome/free-regular-svg-icons'
+import { faTrash, faEdit, faPlay, faGlobe } from '@fortawesome/free-solid-svg-icons'
 
 /* eslint-disable */
 const MOCK_SLIDES = [
@@ -48,27 +49,47 @@ const MOCK_SLIDES = [
 /* eslint-enable */
 
 const SortableItem = SortableElement(({ value }) => (
-  <div className='rectangle'>
-    <div className='rectangle1'>
-      <img className='bitmap' src={value.data} />
-    </div>
-    <div className='rectangle2'>
-      <div className='title'>{value.title}</div>
-      <div className='duration'>
-        {value.duration}s
-        <FontAwesomeIcon icon={faClock} fixedWidth color='#adadad' />
+  <div className='card'>
+    <div className='order'>{value.order}</div>
+    <div className='left'>
+      <div
+        className={'thumbnail'}
+        style={{
+          backgroundImage: `url(${value.data})`,
+          backgroundColor:
+            value.type == 'youtube' ? '#c23616' : value.type == 'web' ? '#0097e6' : 'transparent'
+        }}
+      >
+        {value.type == 'youtube' && (
+          <FontAwesomeIcon icon={faPlay} fixedWidth size='lg' color='#FFFFFF' />
+        )}
+        {value.type == 'web' && (
+          <FontAwesomeIcon icon={faGlobe} fixedWidth size='lg' color='#FFFFFF' />
+        )}
       </div>
     </div>
-    <div className='rectangle3'>
-      <FontAwesomeIcon icon={faEdit} fixedWidth color='#adadad' />
-      <FontAwesomeIcon icon={faTrash} fixedWidth color='#adadad' />
+    <div className='middle'>
+      <div className='title'>{value.title}</div>
+      <div className='duration'>
+        <div className='icon'>
+          <FontAwesomeIcon icon={faClock} fixedWidth color='#878787' />
+        </div>
+        <span className='text'>{value.duration}s</span>
+      </div>
+    </div>
+    <div className='right'>
+      <div className='actionIcon'>
+        <FontAwesomeIcon icon={faEdit} fixedWidth color='#828282' />
+      </div>
+      <div className='actionIcon'>
+        <FontAwesomeIcon icon={faTrash} fixedWidth color='#828282' />
+      </div>
     </div>
     <style jsx>
       {`
-        .rectangle {
-          padding: 20px;
+        .card {
+          padding: 12px;
           font-family: 'Open Sans', sans-serif;
-          text-align: center;
           border-radius: 4px;
           cursor: pointer;
           background: white;
@@ -76,29 +97,87 @@ const SortableItem = SortableElement(({ value }) => (
           margin-bottom: 40px;
           display: flex;
           flex-direction: row;
-          justify-content: space-around;
-        }
-        .rectangle1 {
-          font-family: 'Open Sans', sans-serif;
-          justify-content: space-around;
-        }
-        .rectangle2 {
-          display: flex;
-          flex-direction: column;
-          font-family: 'Open Sans', sans-serif;
-          justify-content: space-around;
+          justify-content: center;
+          position: relative;
+          z-index: 1;
         }
 
-        .rectangle3 {
+        .order {
+          font-family: 'Open Sans', sans-serif;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          padding-left: 8px;
+          padding-right: 8px;
+          color: #878787;
+        }
+
+        .title {
+          font-family: 'Open Sans', sans-serif;
+          font-size: 16px;
+          overflow: hidden;
+          white-space: nowrap;
+          text-overflow: ellipsis;
+          color: #4f4f4f;
+        }
+
+        .left {
+          font-family: 'Open Sans', sans-serif;
+          justify-content: center;
+          padding-left: 8px;
+          padding-right: 8px;
+        }
+
+        .duration {
+          font-family: 'Open Sans', sans-serif;
+          font-size: 14px;
+          color: #878787;
+        }
+
+        .duration .icon {
+          margin-right: 4px;
+          display: inline;
+          vertical-align: middle;
+        }
+
+        .duration .text {
+          vertical-align: middle;
+        }
+
+        .middle {
+          font-family: 'Open Sans', sans-serif;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          padding-left: 8px;
+          padding-right: 8px;
+          flex: 1;
+          min-width: 0;
+        }
+
+        .right {
           display: flex;
           flex-direction: row;
           font-family: 'Open Sans', sans-serif;
           justify-content: center;
+          align-items: center;
+          padding-left: 8px;
+          padding-right: 8px;
         }
 
-        .bitmap {
+        .thumbnail {
           height: 60px;
           width: 60px;
+          border-radius: 2px;
+          background-size: cover;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+
+        .actionIcon {
+          margin-right: 8px;
+          margin-left: 8px;
         }
       `}
     </style>
@@ -107,10 +186,29 @@ const SortableItem = SortableElement(({ value }) => (
 
 const SortableList = SortableContainer(({ items }) => {
   return (
-    <div>
+    <div className={'list'}>
+      <div className={'timeline'} />
       {items.map((value, index) => (
         <SortableItem key={`item-${index}`} index={index} value={value} />
       ))}
+      <style jsx>
+        {`
+          .list {
+            position: relative;
+          }
+          .timeline {
+            width: 4px;
+            height: calc(100% - 20px);
+            border-radius: 2px;
+            position: absolute;
+            left: 50%;
+            top: 10px;
+            margin-left: -2px;
+            background: #cccccc;
+            z-index: 0;
+          }
+        `}
+      </style>
     </div>
   )
 })
@@ -128,7 +226,5 @@ class List extends Component {
     return <SortableList items={this.state.items} onSortEnd={this.onSortEnd} lockAxis='y' />
   }
 }
-
-//render(<List />, document.getElementById("root"))
 
 export default List
