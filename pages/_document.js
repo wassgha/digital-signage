1d44498f4e71ec457386aa539a05cc4bc2391c77
Wassim Gharbi <wassgha@gmail.com>
2019-04-09 17:36:51 -0400

    Adds back a css attribute

--- pages/_document.js
+++ pages/_document.js
@@ -19,6 +19,7 @@ class AppDocument extends Document {
     return (
       <html>
         <Head>
+          <style>{'body { margin: 0 } /* custom! */'}</style>
           <meta name='viewport' content='width=device-width, initial-scale=1' />
           <meta charSet='utf-8' />
           <link
